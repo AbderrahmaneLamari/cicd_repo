16dd615998eab39a6c6da2e48799ae05480fe193
Abderrahmane Lamari <rahmanolam@gamil.com>
2024-11-23 16:15:01 +0100

    Added all test

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const app = require('./app'); // Import the app
 const PORT = 3000;
 
+
+
+app.get('/')
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
--- testSetup.js
+++ testSetup.js
@@ -1,4 +1,6 @@
 const app = require('./app'); // Import the app
+const request = require('supertest');
+
 const TEST_PORT = 3001;
 
 let server;
